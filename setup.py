80d1e992c5eb8dc0a8ae046e92a3ddc2a53dc871
Chris Mason <57402469+cmason3@users.noreply.github.com>
2026-01-09 12:00:09 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
   license="MIT",
   classifiers=[
     "Development Status :: 4 - Beta",
+    "License :: OSI Approved :: MIT License",
     "Programming Language :: Python :: 3"
   ],
   packages=["cvproxy"],
